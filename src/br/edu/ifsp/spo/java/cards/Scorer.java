bcbe177048cc6a7ff0549f661ef8307e464122d6
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2025-05-27 08:18:00 -0300

    Delete src/br/edu/ifsp/spo/java/cards/Scorer.java

--- src/br/edu/ifsp/spo/java/cards/Scorer.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifsp.spo.java.cards;
-
-import java.util.List;
-//import br.edu.ifsp.spo.java.cards.items.Card;
-
-public interface Scorer {
-    int calculateScore(List<Card> hand);
-}
